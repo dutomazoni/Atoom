885ea2b532cb398a2cf6605bfff0bb4e3b1aa117
Dudut <Dudut@DESKTOP-EUF096S>
2020-11-23 13:35:56 -0300

    first commit
    load podcast from db

--- src/dbConnection/mySQLConnection.java
+++ src/dbConnection/mySQLConnection.java
@@ -18,15 +18,38 @@ public class mySQLConnection {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "2002";
     private static final String CONN_STRING = "jdbc:mysql://localhost:3306/atoom_test?useTimezone=true&serverTimezone=UTC";
-
-public static void main(String[] args) throws ClassNotFoundException {
-    Connection conn = null;
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
-        System.out.println("Connected");
-    }catch (SQLException e){
-    System.err.println(e);
+    
+    public static void dbConnect(String user, String pass, String conn_string) throws ClassNotFoundException
+    {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("Connected");
+        }catch (SQLException e){
+            System.err.println(e);
+        }
     }
-}
+    
+    public static void main(String[] args) throws ClassNotFoundException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("Connected");
+        }catch (SQLException e){
+            System.err.println(e);
+        }
+    }
+
+    public mySQLConnection(String user, String pass, String db_url) throws ClassNotFoundException {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(CONN_STRING,USERNAME,PASSWORD);
+            System.out.println("Connected");
+        }catch (SQLException e){
+            System.err.println(e);
+        }
+    }
 }
